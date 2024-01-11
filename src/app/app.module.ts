923a91075426f0db4278b6fff3bb0b9a1687a68f
Fábio Bezerra <bezerradeoliveirafabio@gmail.com>
2024-01-11 17:23:21 -0300

    rascunho dos componentes, inicio do login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovielistViewComponent } from './movielist-view/movielist-view.component';
+import { MovielistItemComponent } from './movielist-item/movielist-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovielistViewComponent,
+    MovielistItemComponent,
+    SearchbarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
