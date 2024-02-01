f7f744a793937bb71375053718e3273d705d46b5
Fábio Bezerra <bezerradeoliveirafabio@gmail.com>
2024-02-01 20:02:31 -0300

    correção

---
+++ OurMovieList-Back/Services/tmdbApiService.cs
@@ -0,0 +1,32 @@
+using System.Net.Http.Headers;
+
+namespace OurMovieList.Services
+{
+    public class tmdbApiService
+    {
+
+        public HttpClient client = new HttpClient();
+        private readonly IConfiguration configuration;
+        public async Task<string> request()
+        {
+            var request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://api.themoviedb.org/3/authentication"),
+                Headers = {
+            {"accept","application/json"},
+            // {"Authorization","authkey"}
+            },
+            };
+
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                                Console.WriteLine(body);
+
+                return body;
+            }
+        }
+    }
+}
